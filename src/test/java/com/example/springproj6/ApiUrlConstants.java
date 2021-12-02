266ef5b11309bbe1d9b9ec4f9fa64121d2583bb9
Elkhan <elxankhabib@gmail.com>
2021-12-02 17:40:12 -0500

    Update ApiUrlConstants.java

--- src/test/java/com/example/springproj6/ApiUrlConstants.java
+++ src/test/java/com/example/springproj6/ApiUrlConstants.java
@@ -3,7 +3,7 @@ package com.example.springproj6;
 public class ApiUrlConstants {
 
     public final static String FIREBASE_SIGNIN_EMAIL_AND_PASSWORD = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=";
-    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/springproj3-4136c/databases/(default)/documents/users";
-    public final static String FIREBASE_STORAGE_AVATAR = "https://firebasestorage.googleapis.com/v0/b/springproj3-4136c.appspot.com/o";
+    public final static String FIRESTORE_DOCUMENT = "https://firestore.googleapis.com/v1/projects/oceanic-abacus-333921/databases/(default)/documents/users";
+    public final static String FIREBASE_STORAGE_AVATAR = "https://firebasestorage.googleapis.com/v0/b/oceanic-abacus-333921.appspot.com/o";
 
 }
