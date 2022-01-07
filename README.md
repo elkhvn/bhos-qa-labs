cf20c9a23c45766c4816ee9a072e507ce7a85b96
Khaaaan <elxankhabib@gmail.com>
2022-01-08 02:01:32 +0400

    Initial !commit q

--- README.md
+++
@@ -1,9 +0,0 @@
-# bhos-qa-labs
-
-
-
-Name: Hajiahmad Ahmadzada
-
-Year: 4th
-
-Major: Information Security
--- src/test/java/com/example/springproj5/User.java
+++
@@ -1,7 +0,0 @@
-package com.example.springproj5;
-
-public class User {
-    public Object name;
-    public Object surname;
-    public Object phone;
-}
